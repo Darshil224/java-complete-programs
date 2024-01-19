d4768e054d1acd163aa172fa1bb8bfdc0b529b64
Darshil224 <darshilchitranshi2204@gmail.com>
2024-01-19 19:45:44 +0530

    further updated pattern programs

--- JAVA PRACTICE PROGRAMS/1loopstarpattern.java
+++ JAVA PRACTICE PROGRAMS/1loopstarpattern.java
@@ -1,6 +1,6 @@
  class loopstarpattern {
     public static void main(String args[]){
-        //right half pyramid 5 rows
+        // 1)right half pyramid 5 rows
         // for(int i=1;i<=5;i++){
         //     for(int j=1;j<=i;j++){
         //         System.out.print("*");
@@ -17,7 +17,7 @@
         //     ****
         //     *****
          
-        //reverse right half pyramid 5 rows
+        //2)reverse right half pyramid 5 rows
 
 
         // for(int i=5;i>=1;i--){
@@ -35,7 +35,7 @@
         // **
         // *
 
-        // square n*n
+        // 3)square n*n
 
         // int n=5; //no of rows and columns
         // for(int i=1;i<=5;i++){
@@ -53,7 +53,7 @@
         // *****
 
 
-        //rectangle r*c r=row,c=columns
+        //4)rectangle r*c r=row,c=columns
 
         // int r=4;
         // int c=7;
@@ -70,7 +70,7 @@
         // *******
         // *******
 
-        //left half pyramid
+        //5)left half pyramid
 
         // int r=5;
         // for(int i=1;i<=r;i++){
@@ -91,7 +91,7 @@
         // *****
      
 
-        //revrse left half pyramid r rows
+        //6)revrse left half pyramid r rows
 
 
         // int r=5;
@@ -115,7 +115,7 @@
             // 111**
             // 1111*
 
-            //pyramid(triangle) r rows
+            //7)pyramid(triangle) r rows
 
             // int r=6;
 
@@ -148,6 +148,118 @@
             // 1****
             // *****
 
+            //8)inverse pyramid/triangle
+            // int r=5;
+
+            // for (int i = 1; i <=r; i++) {
+            //     for (int j = 1; j <i ; j++) {
+            //         System.out.print("1");
+                    
+            //     }
+            //     for (int k = r; k>=i; k--) {
+            //         System.out.print("* "); //notice the space given
+                    
+            //     }
+            //     System.out.println();
+                
+            // }
+
+
+            // output:-
+
+            // * * * * * 
+            // 1* * * * 
+            // 11* * * 
+            // 111* *
+            // 1111*
+
+            //9) Diamond
+
+
+            // int r=6;
+
+            // for (int i = 1; i <=r; i++) {
+            //     for (int j = r; j > i; j--) {
+            //         System.out.print("1");
+                    
+            //     }
+            //     for (int k = 1; k<=i; k++) {
+            //         System.out.print("* "); //notice the space given after * , that is very very important here
+                    
+            //     }
+            //     System.out.println();  // ending line after each row
+                
+            // }
+                
+
+            // for (int i = 1; i <=r; i++) {
+            //     for (int j = 1; j <=i ; j++) {
+            //         System.out.print("1");
+                    
+            //     }
+            //     for (int k = r; k>i; k--) {
+            //         System.out.print("* "); //notice the space given
+                    
+            //     }
+            //     System.out.println();
+                
+            // }
+
+            //right half diamond
+
+            // int r=5;
+            // for (int i = 1; i <=r; i++) {
+            //     for (int j = 1; j<=i; j++) {
+            //         System.out.print("*");
+                    
+            //     }
+                
+            //     System.out.println();
+            // }
+            // for (int i = 1; i <r; i++) {
+            //     for (int k = i; k <r ; k++) {
+            //         System.out.print("*");
+            // }
+            //     System.out.println();
+                
+            // }
+
+            // output:
+
+            // *
+            // **
+            // ***
+            // ****
+            // *****
+            // ****
+            // ***
+            // **
+            // *
+
+            //left jalf diamond
+            int r=5;
+           
+                for (int i = 1; i <=r; i++) {
+                    for (int j = i; j <r; j++) {
+                        System.out.print(" ");
+                        
+                    }
+                    for (int k = 1; k <=i; k++) {
+                        System.out.print("*");
+                        
+                    }
+                    System.out.println();
+
+                    
+                }
+                for (int i = 0; i < args.length; i++) {
+                    
+                }
+            
+
+
+
+            
 
 
 
