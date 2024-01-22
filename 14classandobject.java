d8c65a8c6a097d74ab639a872315ea0f75584c6b
Darshil224 <darshilchitranshi2204@gmail.com>
2024-01-22 23:56:24 +0530

    learned about class and objects in java

--- 14classandobject.java
+++ 14classandobject.java
@@ -1,3 +1,36 @@
-public class 14classandobject {
+class Calculator{
+    int a; //variable
+    public int add(int n1, int n2){ //method
+
+    //public=can be call from anywhere else(this is for method) ,int=return type, add is the method name
+
+    int r= n1+n2;
+    return r;
+
+    }
+
+}
+
+
+  class Demo{
+    public static void main(String args[])
+{
+    int num1=4;
+    int num2=5;
+
+    // int result= num1 + num2; //but we dont want to add these numbers, we want to call add function and add any 2 numbers (like apple design in california but assemble in china, designing is more useful)
+
+    // System.out.println(result);
+
+    Calculator calc = new Calculator(); //creating an object
+    // calc is reference variable of Calculator data type
+    int result =calc.add(4,5);
+    System.out.println(result);
+}
+
+
+
+    
     
 }
+//every object has two things- properties and methods(action/behavior)
