46779a673b38f8b529771c1020cd12f93ef96036
Darshil224 <darshilchitranshi2204@gmail.com>
2024-02-03 23:46:44 +0530

    final keyword

--- 42dynamicmethoddispatch.java
+++ 42dynamicmethoddispatch.java
@@ -29,7 +29,9 @@ class Demo{
           obj = new C();//new C() indicates obj is a object of class C. still obj is of A data type, It is working because A is parent of C
         obj.show();
 
-        //this is an example of run time polymorphism. And all this methods are called dynamic method dispatch because it is dynamic, which method it will call we are not sure.
+        //obj.show()is behaving differently with different objects -- polymorphism.
+        //irrespective of what type of variable you create, it only depends upon what object you have.( only possible in inheritance) 
+        //this is an example of run time polymorphism. And all this concept are called dynamic method dispatch because it is dynamic, which method it will call we are not sure.
 
 
    }
