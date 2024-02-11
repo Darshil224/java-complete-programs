316a42c8c56a124cb25dadd322eb718b2ddaf153
Darshil224 <darshilchitranshi2204@gmail.com>
2024-02-11 23:13:08 +0530

    multiple threads

--- 69trywithresources.java
+++ 69trywithresources.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
   class Demo {
-    public static void main(String[] args) throws NumberFormatException {   
+    public static void main(String[] args) throws NumberFormatException, IOException {   
     	
     	int i=0;
     	int j=0;
